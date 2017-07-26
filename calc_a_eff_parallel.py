Correct error calculation and add sanity checks in a_eff calculation.

calc_a_eff_parallel.py
@@ -71,10 +71,22 @@ def calc_a_eff_parallel(ebins, zdbins, use_mc=True, theta_square_cut="0.085", pa
         len(zdbins) - 1) + "emc_" + str(use_mc) + "theta_sq" + theta_square_cut
 
     if os.path.isfile(name + ".npy"):
-        print("    Numpy file of effective area exists. Trying to read it.")
+        print("    Numpy file of effective area exists. Loading it.")
         a_eff = np.load(name + ".npy")
+        print(np.mean(a_eff))
+        print(np.median(a_eff))
         if not (a_eff.shape == (len(zdbins) - 1, len(ebins) - 1)):
-            print("    Shape of effective area is wrong, please delete the old file and run again:", name)
+            print("    Shape of effective area is wrong, removing", name, "...")
+            os.remove(name + ".npy")
+            print("    Please run script again.")
+        elif (np.isinf(np.mean(a_eff))):
+            print("    Median of effective area is Inf, removing",name, "...")
+            os.remove(name + ".npy")
+            print("    Please run script again.")
+        elif (np.isnan(np.median(a_eff))):
+            print("    Median of effective area is NaN, removing",name, "...")
+            os.remove(name + ".npy")
+            print("    Please run script again.")
     else:
         print("    Loading surviving gammas.")
         Events = ROOT.TChain("Events")
@@ -102,6 +114,23 @@ def calc_a_eff_parallel(ebins, zdbins, use_mc=True, theta_square_cut="0.085", pa
         if os.path.isfile(mc_name + ".npy"):
             print("    Loading existing histogram of simulated Gammas in correct binning:", mc_name+".npy")
             n_mc = np.load(mc_name + ".npy")
+            if not (n_mc.shape == (len(zdbins) - 1, len(ebins) - 1)):
+                print("    Shape of existing histogram is wrong, removing", mc_name, ".npy ...")
+                os.remove(mc_name + ".npy")
+                print("    Please run script again.")
+            elif (np.isinf(np.mean(n_mc))):
+                print("    Median of histogram is Inf, removing", mc_name, ".npy ...")
+                os.remove(mc_name + ".npy")
+                print("    Please run script again.")
+            elif (np.isnan(np.median(n_mc))):
+                print("    Median of histogram is NaN, removing", mc_name, ".npy ...")
+                os.remove(mc_name + ".npy")
+                print("    Please run script again.")
+            elif (np.mean(n_mc) == 0):
+                print("    Mean of histogram is 0, removing", mc_name, ".npy ...")
+                os.remove(mc_name+".npy")
+                print("    Please run script again.")
+
         else:
             print("    Calculating histogram of simulated Gammas from callisto root files.")
             n_mc_parts = np.zeros([n_chunks, len(zdbins) - 1, len(ebins) - 1])

on_time_parallel.py
@@ -64,7 +64,7 @@ def calc_on_time(ganymed_input_list, zdbins, zdlabels, n_chunks=8):
     parts = (len(ganymed_input_list) / n_chunks * np.arange(n_chunks + 1)).astype("int_")
 
     print("\nOn time calculation ---------")
-    print("Calculate the on time in", n_chunks, "chunks with", len(ganymed_input_list), "entries each.")
+    print("Calculate the on time in", n_chunks, "chunks with a total of", len(ganymed_input_list), "entries.")
 
     on_time_parts = np.empty([n_chunks, len(zdbins) - 1])
 

spectrum_eff_eff_area.py
@@ -35,16 +35,21 @@ if __name__ == '__main__':
 
     thetasq = 0.026
     zdbins = np.linspace(0, 60, 15)
-    ebins = np.logspace(np.log10(200.0), np.log10(50000.0), 11)
+    ebins = np.logspace(np.log10(200.0), np.log10(50000.0), 10)
     use_mc = False
-    star_list = list(open("/media/michi/523E69793E69574F/daten/hzd_mrk501.txt", "r"))
+    star_files = ["/media/michi/523E69793E69574F/daten/hzd_mrk501.txt"]
     # On ISDC, put None, to read automatically processed Ganymed Output from star files:
-    ganymed_result = None
+    ganymed_result = "/media/michi/523E69793E69574F/daten/hzd_mrk501-analysis.root"
     base_path = "/media/michi/523E69793E69574F/daten/"
 
     # Create the labels for binning in energy and zenith distance.
     zdlabels = range(len(zdbins) - 1)
     elabels = range(len(ebins) - 1)
+
+    # Iterate over a list of input STAR files:
+    star_list = []
+    for entry in star_files:
+        star_list += list(open(entry, "r"))
 
     # Calcualtion of on time from ganymed input list of star files:
 
@@ -127,15 +132,15 @@ if __name__ == '__main__':
         [2.574657e-10, 3.90262e-11, 4.364535e-12, 1.083812e-12, 3.51288e-13, 1.207386e-13, 4.346247e-14, 1.683129e-14,
          6.689311e-15])
 
-    flux_de = (flux / (bin_width / 1000))
-    flux_de_err = ((flux_err / (bin_width / 1000)) / (flux_de * np.log(10)))
+    flux_de = np.divide(flux, np.divide(bin_width, 1000))
+    flux_de_err = np.divide(flux_err,np.divide(bin_width, 1000)) # / (flux_de * np.log(10))
     flux_de_err_log10 = symmetric_log10_errors(flux_de, flux_de_err)
 
     plt.figure("Spectrum")
     ax1 = plt.subplot(121)
 
-    plt.errorbar(x=bin_centers, y=flux_de, yerr=flux_de_err_log10, xerr=[bin_centers-ebins[:-1], ebins[1:]-bin_centers],
-                 fmt=".", label="FACT")
+    plt.errorbar(x=bin_centers, y=flux_de, yerr=flux_de_err_log10, xerr=[bin_centers-ebins[:-1], ebins[1:]-bin_centers]
+                 , fmt=".", label="FACT")
     plt.errorbar(x=hess_x * 1000, y=hess_y, yerr=[hess_yl, hess_yh], fmt=".", label="HESS 24.06.2014")
     # plt.errorbar(x=crab_do_x, y=crab_do_y, yerr=crab_do_y_err, xerr=[crab_do_x-crab_do_x_l,crab_do_x_h-crab_do_x],
     #  fmt="o", label="Crab Dortmund")
@@ -172,24 +177,24 @@ if __name__ == '__main__':
     gs = gspec.GridSpec(2, 4, height_ratios=[1, 4])
 
     ax5 = fig.add_subplot(gs[1, 0])
-    im = ax5.imshow(a_eff, extent=[np.log10(200), np.log10(50000), 0, 60], aspect=0.1, origin='lower')
+    im = ax5.imshow(a_eff, extent=[np.log10(200), np.log10(50000), 0, 60], aspect=0.1, origin='lower', interpolation=None)
     plt.ylabel('Zenith Distance [deg]')
     plt.xlabel('log10(E [GeV])')
     plt.title('Effective Area')
     plt.colorbar(im, ax=ax5)
 
     fig.add_subplot(gs[1, 1])
-    plt.imshow(exc_histo, extent=[np.log10(200), np.log10(50000), 0, 60], aspect=0.1, origin='lower')
+    plt.imshow(exc_histo, extent=[np.log10(200), np.log10(50000), 0, 60], aspect=0.1, origin='lower', interpolation=None)
     plt.title('Excess Events')
     plt.colorbar()
 
     fig.add_subplot(gs[1, 2])
-    plt.imshow(on_histo, extent=[np.log10(200), np.log10(50000), 0, 60], aspect=0.1, origin='lower')
+    plt.imshow(on_histo, extent=[np.log10(200), np.log10(50000), 0, 60], aspect=0.1, origin='lower', interpolation=None)
     plt.title('On Events')
     plt.colorbar()
 
     fig.add_subplot(gs[1, 3])
-    plt.imshow(off_histo*0.2, extent=[np.log10(200), np.log10(50000), 0, 60], aspect=0.1, origin='lower')
+    plt.imshow(off_histo*0.2, extent=[np.log10(200), np.log10(50000), 0, 60], aspect=0.1, origin='lower', interpolation=None)
     plt.title('Off Events')
     plt.colorbar()
 