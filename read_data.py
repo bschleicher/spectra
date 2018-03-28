bbc391aa4795f206a91363f2d57c743bbdaafdc3
Michael Blank <michael.blank@stud-mail.uni-wuerzburg.de>
2018-03-28 22:37:24 +0200

    Change hardcoded filenames to specified list of filenames

--- read_data.py
+++ read_data.py
@@ -4,13 +4,19 @@ import read_mars
 from multiprocessing import Pool
 
 
-
-def read_and_select_data(entry, tree_name, leafs, zdbinsr, zdlabelsr, ebinsr, elabelsr, thetasqr):
+def read_data_and_bg_cut(entry, tree_name, leafs):
     temp = read_mars.read_mars(entry, tree=tree_name, leaf_names=leafs)
     # Apply Johannes Cut: Area < log10(Size) * 898 -1535
     temp = pd.DataFrame(temp[temp.apply(
         lambda x: (np.pi * x['MHillas.fLength'] * x['MHillas.fWidth']) < (np.log10(x['MHillas.fSize']) * 898 - 1535),
         axis=1)], columns=leafs)
+
+    return temp
+
+
+def read_and_select_data(entry, tree_name, leafs, zdbinsr, zdlabelsr, ebinsr, elabelsr, thetasqr):
+    temp = read_data_and_bg_cut(entry, tree_name, leafs)
+
     return calc_onoffhisto(temp, zdbinsr, zdlabelsr, ebinsr, elabelsr, thetasqr)
 
 
@@ -35,7 +41,6 @@ def calc_onoffhisto(data, zdbins, zdlabels, ebins, elabels, thetasq):
     data = data.assign(energy=lambda x: (np.power(29.65 * x["MHillas.fSize"],
                                                   (0.77 / np.cos((x["MPointingPos.fZd"] * 1.35 * np.pi) / 360))) +
                                          x["MNewImagePar.fLeakage2"] * 13000))
-
     on_histo = np.zeros([len(zdlabels), len(elabels)])
     off_histo = np.zeros([len(zdlabels), len(elabels)])
 
@@ -53,10 +58,10 @@ def calc_onoffhisto(data, zdbins, zdlabels, ebins, elabels, thetasq):
             on_histo = np.histogram2d(on_data["MPointingPos.fZd"], on_data["energy"], bins=[zdbins, ebins])[0]
             off_histo = np.histogram2d(off_data["MPointingPos.fZd"], off_data["energy"], bins=[zdbins, ebins])[0]
             on_thetasq = np.histogram(data.groupby("DataType.fVal").get_group(1.0)["ThetaSquared.fVal"],
-                                      bins = 40, range=(0.0,0.3))
+                                      bins=40, range=(0.0, 0.3))
             off_thetasq = np.histogram(data.groupby("DataType.fVal").get_group(0.0)["ThetaSquared.fVal"],
-                                       bins = 40, range=(0.0,0.3))
+                                       bins=40, range=(0.0, 0.3))
         except:
             on_histo = np.zeros([len(zdlabels), len(elabels)])
             off_histo = np.zeros([len(zdlabels), len(elabels)])
-    return (np.array([on_histo, off_histo]),[on_thetasq, off_thetasq])
+    return np.array([on_histo, off_histo]), [on_thetasq, off_thetasq]
