61cd3b11384331d8cc1e055180ae0efe64e31e4d
Michael Blank <michael.blank@stud-mail.uni-wuerzburg.de>
2018-10-10 12:46:01 +0200

    Example also performs a fit.

--- example.py
+++ example.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from blockspec.spec import Spectrum
+from blockspec.spec.energy_functions import forest_energy_length
 
 from blockspec.spec.calc_a_eff_parallel import calc_a_eff_parallel_hd5
 
@@ -13,10 +14,12 @@ for i in range(8):
 
 # Monte Carlo surviving events:
 
-ganymed_mc = "/media/michi/523E69793E69574F/daten/gamma/hzd_gammasall-analysis.root"
+#ganymed_mc = "/media/michi/523E69793E69574F/daten/gamma/hzd_gammasall-analysis.root"
 ganymed_mc = "/home/michi/ml/mc/star/dortmund_disp2018-analysis.root"
 # Star files of data:
 
+star_files = ["/home/michi/data/bump/july19_21.txt"]
+#star_files = ["/home/michi/data/threshold/blocks/(289.097, 402.875].txt"]
 star_files = ["/home/michi/data/mrk501daily/blocks/20140623.txt"]
 #star_files = ["/media/michi/523E69793E69574F/daten/crab2.txt"]
 #star_files = ["/media/michi/523E69793E69574F/daten/Mrk501/blocks/20140623__20140623.txt"]
@@ -28,8 +31,10 @@ star_list = []
 for entry in star_files:
     star_list += list(open(entry, "r"))
 
-# Ganymed result:
 
+# Ganymed result:
+ganymed_result = "/home/michi/data/bump/july19_21-analysis.root"
+#ganymed_result = "/home/michi/data/threshold/(289.097, 402.875]-analysis.root"
 ganymed_result = "/home/michi/data/mrk501daily/20140623-analysis.root"
 #ganymed_result = "/media/michi/523E69793E69574F/daten/crab2-analysis.root"
 #ganymed_result = "/media/michi/523E69793E69574F/daten/Mrk501/19-analysis.root"
@@ -43,39 +48,6 @@ spectrum = Spectrum(run_list_star=star_list,
                     list_of_ceres_files=ceres_list,
                     ganymed_file_mc=ganymed_mc)
 # spectrum.set_correction_factors()
-
-
-def forest_energy_impact(gamma_on):
-    from sklearn.externals import joblib
-
-    # clf = joblib.load('/home/michi/random_forest_impact.pkl')
-    clf = joblib.load('/home/michi/ml/mc/star/forest_impact_disp2018.pkl')
-    training_variables_impact = ['MPointingPos.fZd', 'MHillas.fLength', 'MHillas.fSize', 'MHillas.fWidth',
-                                 'MHillasSrc.fDist', 'MHillasExt.fM3Long', 'MHillasExt.fSlopeLong',
-                                 'MHillasExt.fSlopeSpreadWeighted', 'MHillasExt.fTimeSpreadWeighted',
-                                 'MHillasSrc.fCosDeltaAlpha']
-    X = gamma_on[training_variables_impact]
-
-    predicted = clf.predict(X)
-
-    gamma_on["impact"] = predicted
-
-    clf = joblib.load('/home/michi/ml/mc/star/forest_impact_energy2018.pkl')
-
-    training_variables = [
-                          'MPointingPos.fZd',
-                          'MHillas.fSize',
-                          'MNewImagePar.fLeakage2',
-                          'MHillas.fWidth',
-                          'MHillasSrc.fDist',
-                          'MHillasExt.fM3Long',
-                          'MHillasExt.fSlopeLong',
-                          'impact'
-                         ]
-    X = gamma_on[training_variables]
-
-    predicted = clf.predict(X)
-    return predicted
 
 
 def forest_energy(gamma_on):
@@ -105,8 +77,8 @@ def cut_function(x):
     b = size > 125
     return np.logical_and(a, b)
 
-ebins = np.logspace(np.log10(200), np.log10(50000), 41)
-zdbins = np.linspace(0, 60, 21)
+ebins = np.logspace(np.log10(200), np.log10(50000), 31)
+zdbins = np.linspace(0, 60, 31)
 
 # areas = calc_a_eff_parallel_hd5(ebins, zdbins, False,
 #                                0.04, path=ganymed_mc, list_of_hdf_ceres_files=ceres_list,
@@ -120,14 +92,18 @@ spectrum.set_theta_square(0.04)
 #spectrum.optimize_ebinning(min_counts_per_bin=10, sigma_threshold=3, min_bin_percentage=0.2)
 spectrum.set_energy_binning(ebins)
 spectrum.set_zenith_binning(zdbins)
-spectrum.set_correction_factors(False)
+#spectrum.set_correction_factors(True)
 spec = spectrum.calc_differential_spectrum(use_multiprocessing=False,
-                                    efunc=forest_energy_impact,
-                                    # slope_goal=0.3,
-                                    cut=cut,
-                                    )
+                                           efunc=forest_energy_length,
+                                           # slope_goal=0.3,
+                                           #cut=cut,
+                                           )
 print(spec)
 print(spectrum.energy_migration)
+print(spectrum.on_histo)
+print(spectrum.off_histo)
+print(spectrum.excess_histo)
+print(spectrum.excess_histo_err)
 
 spectrum.plot_flux(crab_magic=True, hess_flare=True)
 spectrum.plot_thetasq()
@@ -150,7 +126,18 @@ labels = ["$\Phi$ [cm^{-2}s^{-1}TeV^{-1}]", "$\Gamma$"]
 names = ["flux", "index"]
 
 
-fit = fit_ll(second, model=powerlaw, bounds=bounds, labels=labels, names=names, nsamples=500)
+def powerlaw(x, k, gamma, b, mu, sigma):
+    k = np.power(10, k)
+    b = np.power(10, b)
+    gaussian = np.divide(b, sigma) * np.exp(-np.divide(x/1000 - mu,4*sigma)**2)
+    return k*np.power(x/1000, gamma) + gaussian # * np.exp(np.divide(x, 6000))
+
+bounds = [[-20, -5], [-8, 1], [-17, -5], [3, 5], [0, 30000]]
+labels = ["$\Phi$ [cm^{-2}s^{-1}TeV^{-1}]", "$\Gamma$", "b", "$\mu$", "$\sigma$"]
+names = ["flux", "index", "b", "mu", "sigma"]
+
+
+fit = fit_ll(second, model=powerlaw, bounds=bounds, labels=labels, names=names, nsamples=1500)
 print(fit)
 
 n = np.sum(0.2 * second.off_histo_zenith > 0)
@@ -158,12 +145,22 @@ print(n)
 
 from corner import corner
 
-corner(fit["samples"][:,200:,:].reshape(-1,2), show_titles=True)
+corner(fit["samples"][:,200:,:].reshape(-1,len(labels)), show_titles=True)
 
-corner(fit["samples"][:,150:,:].reshape(-1,2), show_titles=True)
+corner(fit["samples"][:,150:,:].reshape(-1,len(labels)), show_titles=True)
 
 second.plot_flux(hess_flare=True)
+
+ax = plt.gca()
+from blockspec.block.block import conf_from_sample
+
+x, mid, low, up = conf_from_sample(fit["samples"][:,150:,:], powerlaw)
+ax.plot(x, mid, label="Fit LogLike")
+ax.fill_between(x, low, up, label="LogLike 68 % confidence", alpha=0.4)
+
+
 second.plot_thetasq()
+
 
 
 
