ddb3639e56e26279ee7909c50a92a3e07a7528ea
bschleicher <bernd.schleicher@stud-mail.uni-wuerzburg.de>
2020-12-02 13:36:21 +0100

    Update spectrum_class.py
    fix in header effective area file

--- blockspec/spec/spectrum_class.py
+++ blockspec/spec/spectrum_class.py
@@ -494,7 +494,7 @@ class Spectrum:
             np.savetxt(filename+'flux_de_err.txt', (np.vstack((bin_centers,flux_de,bin_error,flux_de_err,flux_de_err)).T),header='bin_centers flux_de, bin_error (low&high), flux_de_error_log10 (low&high)')
             np.savetxt(filename+'flux_de_err_andmore.txt', (np.vstack((bin_centers,flux_de,bin_error,flux_de_err,flux_de_err,self.excess_histo,exc_err)).T),header='bin_centers flux_de, bin_error (low&high), flux_de_error_log10 (low&high),ExcessHisto, exc_err')     
             np.savetxt(filename+'Scaled_EffectiveArea.txt', (np.vstack((bin_centers,self.scaled_effective_area,bin_error,self.scaled_effective_area_err)).T),header='bin_centers scaledEffectiveError, bin_error, scaledEffectiveAreaErr')
-            np.savetxt(filename+'EffectiveArea.txt', (np.vstack((bin_centers,self.effective_area,bin_error,self.effective_area_err)).T),header='bin_centers scaledEffectiveError, bin_error, scaledEffectiveAreaErr')
+            np.savetxt(filename+'EffectiveArea.txt', (np.vstack((bin_centers,self.effective_area,bin_error,self.effective_area_err)).T),header='bin_centers EffectiveError, bin_error, scaledEffectiveAreaErr')
         flux_de_err_log10 = symmetric_log10_errors(flux_de, flux_de_err)
         if filename is not None:
             np.savetxt(filename+'LogErrors.txt', (np.vstack((bin_centers,flux_de,bin_error,flux_de_err_log10)).T),header='bin_centers flux_de, bin_error (low&high), flux_de_error_log10 (low&high)') 
