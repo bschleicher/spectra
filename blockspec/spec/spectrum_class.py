9258f05c3ef81823638e3f9a282274b73683201a
bschleicher <bernd.schleicher@stud-mail.uni-wuerzburg.de>
2020-12-08 14:53:56 +0100

    Update spectrum_class.py
    test effective area

--- blockspec/spec/spectrum_class.py
+++ blockspec/spec/spectrum_class.py
@@ -494,8 +494,8 @@ class Spectrum:
             np.savetxt(filename+'.txt', (np.vstack((bin_centers,flux,bin_error,flux_err,flux_err)).T),header='bin_centers flux_de, bin_error (low&high), flux_de_error_log10 (low&high)')
             np.savetxt(filename+'flux_de_err.txt', (np.vstack((bin_centers,flux_de,bin_error,flux_de_err,flux_de_err)).T),header='bin_centers flux_de, bin_error (low&high), flux_de_error_log10 (low&high)')
             np.savetxt(filename+'flux_de_err_andmore.txt', (np.vstack((bin_centers,flux_de,bin_error,flux_de_err,flux_de_err,self.excess_histo,exc_err)).T),header='bin_centers flux_de, bin_error (low&high), flux_de_error_log10 (low&high),ExcessHisto, exc_err')     
-            np.savetxt(filename+'Scaled_EffectiveArea.txt', (np.vstack((bin_centers,scaled_a_eff,bin_error)).T),header='bin_centers scaledEffectiveError, bin_error (low&high)')
-            np.savetxt(filename+'EffectiveArea.txt', (np.vstack((bin_centers,np.sum(self.effective_area, axis=0),bin_error)).T),header='bin_centers EffectiveError, bin_error (low&high)')
+            np.savetxt(filename+'Scaled_EffectiveArea.txt', (np.vstack((bin_centers,scaled_a_eff)).T),header='bin_centers scaledEffectiveError')
+            np.savetxt(filename+'EffectiveArea.txt', (np.vstack((bin_centers,np.sum(self.effective_area, axis=0))).T),header='bin_centers EffectiveArea')
         flux_de_err_log10 = symmetric_log10_errors(flux_de, flux_de_err)
         if filename is not None:
             np.savetxt(filename+'LogErrors.txt', (np.vstack((bin_centers,flux_de,bin_error,flux_de_err_log10)).T),header='bin_centers flux_de, bin_error (low&high), flux_de_error_log10 (low&high)') 
