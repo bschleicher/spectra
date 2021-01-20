5b66bed2959fd61b32c92898ef2b765f551e14cc
bschleicher <bernd.schleicher@stud-mail.uni-wuerzburg.de>
2021-01-20 17:12:01 +0100

    Update spectrum_class.py

--- blockspec/spec/spectrum_class.py
+++ blockspec/spec/spectrum_class.py
@@ -493,7 +493,7 @@ class Spectrum:
         a_eff_err = np.sqrt(np.sum(self.scaled_effective_area_err**2))
         print(a_eff_err)
         flux_err = flux * np.sqrt(np.ma.divide(exc_err, self.excess_histo)**2 +
-                                  np.ma.divide(a_eff_err, np.sum(self.scaled_effective_area, axis=0))**2)
+                                  np.ma.divide(self.scaled_effective_area_err, self.scaled_effective_area)**2)
 
         flux_de = np.divide(flux, np.divide(bin_width, 1000))
         flux_de_err = np.divide(flux_err, np.divide(bin_width, 1000))  # / (flux_de * np.log(10))
